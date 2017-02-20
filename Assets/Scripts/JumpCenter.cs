bc7497e5bf3dcba96b928504f584bec04f2de879
Robert Fearon <robert.a.fearon@gmail.com>
2017-02-19 22:40:12 -0800

    added starter 3d stage
    script for jump pads is very crude and buggy right now

---
+++ Assets/Scripts/JumpCenter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpCenter : MonoBehaviour
+{
+
+    public float upStrength;
+    public float centerStrength;
+    public Vector3 center; 
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        collision.rigidbody.AddForce(this.gameObject.transform.up * upStrength + this.gameObject.transform.TransformDirection(center) * centerStrength);
+    }
+}
