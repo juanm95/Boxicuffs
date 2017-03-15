b257265f920ebc34059ce840dc5fca3f50969e8e
Robert Fearon <robert.a.fearon@gmail.com>
2017-03-14 17:34:03 -0700

    Added shrink moustache

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class HealthBar : MonoBehaviour {
     public float healthScale;
     public Boxer boxerScript;
+    public GameObject moustache;
+    public Vector3 origMoustacheScale;
 
     // Use this for initialization
     void Start()
     {
         GameObject boxerParent = gameObject.transform.parent.gameObject.transform.parent.gameObject.transform.parent.gameObject;
         boxerScript = boxerParent.GetComponent<Boxer>();
+        moustache = boxerParent.transform.FindChild("moustacheObj").gameObject;
+        origMoustacheScale = moustache.transform.localScale;
     }
     // Update is called once per frame
         void Update () {
@@ -21,6 +25,12 @@ public class HealthBar : MonoBehaviour {
     {
         healthScale = boxerScript.health / 100;
         gameObject.transform.localScale = new Vector3(healthScale, 1, 1);
+        ShrinkMoustache();
+    }
+
+    void ShrinkMoustache()
+    {
+        moustache.transform.localScale =  origMoustacheScale * healthScale;
     }
 
 }
