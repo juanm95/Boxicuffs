69fbcad26815106c7382b039b4107ce1fe4ce8c1
Robert Fearon <robert.a.fearon@gmail.com>
2017-03-18 21:54:05 -0700

    added scene loading

---
+++ Assets/LaunchGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class LaunchGame : MonoBehaviour {
+    public string sceneName;
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Button>().onClick.AddListener(LoadLevel);//
+    }
+
+    void LoadLevel()
+    {
+        SceneManager.LoadSceneAsync(sceneName);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
