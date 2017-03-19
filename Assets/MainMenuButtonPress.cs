25e7655707d2e355805baca7557c41565421294c
Robert Fearon <robert.a.fearon@gmail.com>
2017-03-18 21:40:26 -0700

    Added main menu and tutorial scene

---
+++ Assets/MainMenuButtonPress.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MainMenuButtonPress : MonoBehaviour {
+
+    public Color inactive;
+    public Color active;
+    public string key;
+
+    private Image background;
+
+	// Use this for initialization
+	void Start () {
+        background = gameObject.GetComponent<Image>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            background.color = active;
+        }
+        if (Input.GetKeyUp(key))
+        {
+            background.color = inactive;
+        }
+    }
+
+}
